40eaa78074b51f36bdc126210668f80ad1a45b21
thalles <thalles.nascimentorj@gmail.com>
2023-08-21 10:01:38 -0300

    Exercícios de Python

--- ex001.py
+++ ex001.py
@@ -1,3 +1,5 @@
-name = input('Qual o seu nome? ')
+#Input é a entrada de dados do usuário
+#Print é uma função de imprimir na tela do console o que foi passado
 
+name = input('Qual o seu nome? ')
 print('É um prazer te conhecer,', name, '!')
