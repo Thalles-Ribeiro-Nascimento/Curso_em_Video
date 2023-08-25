0a8b4ef804206e35a345c4abfbc03cf4fd0ffa18
thalles <thalles.nascimentorj@gmail.com>
2023-08-25 13:10:46 -0300

    Fiz algumas alterações

--- desafioAula07g.py
+++ desafioAula07g.py
@@ -7,9 +7,9 @@ width = float(input('Insira a largura da parede(métros): '))
 heigth = float(input('Insira a altura da parede(métros): '))
 
 area = width * heigth
-tinta = int(area /2)
+tinta = float(area /2)
 
-print(f'Sua parede possui uma área de {area}m² e você conseguira pintar uma área de {tinta}m² com uma lata de tinta!')
+print(f'Sua parede possui uma área de {area:.3f}m² e você conseguira pintar uma área de {tinta}m² com uma lata de tinta!')
 
 print('-'*55)
 print('Próximo desafio!!')
