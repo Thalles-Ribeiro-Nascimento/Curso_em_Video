fd9e8f481ae95f2c6f1a384e6e7ba8ad3e77a8d8
thalles <thalles.nascimentorj@gmail.com>
2023-08-25 13:28:47 -0300

    Fiz algumas alterações

--- desafioAula07i.py
+++ desafioAula07i.py
@@ -3,9 +3,9 @@ print('Seja bem-vindo ao nono desafio da aula 07 do Curso em Vídeo!!')
 print('-'*55)
 
 # Aumento de salário
-sal = int(input('Insira seu salário: R$ '))
-perc = sal * 15/100
-aum = sal + perc
+sal = float(input('Insira seu salário: R$ '))
+aum = sal + (sal * 15/100)
+
 
 print(f'Seu salário de R$ {sal:.2f} recebeu um aumento de 15%\n'
       f'Seu novo salário é de R$ {aum:.2f}')
